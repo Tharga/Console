2fc03374a0e3bdb6bb910303f36c588925f4b089
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-02-14 03:50:31 +0100

    Added build configurations for Team City so quicker deploy to nuget.org is possible

--- Tharga.Toolkit.Console/Properties/AssemblyInfo.cs
+++ Tharga.Toolkit.Console/Properties/AssemblyInfo.cs
@@ -22,17 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("00e175b1-6820-4adc-b816-0b3096500e38")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Tharga.Toolkit.Console.Tests")]
