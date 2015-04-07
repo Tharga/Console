a77a4d0f57a4bc6745a37c8864cdabe16eec9284
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-04-08 01:16:04 +0200

    Created an example for engine output that works, and one that does not work.

--- Samples/SampleConsole/Program.cs
+++ Samples/SampleConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Timers;
 using Tharga.Toolkit.Console;
 using Tharga.Toolkit.Console.Command;
 using Tharga.Toolkit.Console.Command.Base;
@@ -11,8 +12,13 @@ namespace SampleConsole
     {
         static void Main(string[] args)
         {
-            var command = new RootCommand(new VoiceConsole());
+            var console = new ClientConsole();
+            //var console = new VoiceConsole();
+            //var console = new ServerConsole(string.Empty);
+
+            var command = new RootCommand(console);
             command.RegisterCommand(new SomeContainerCommand());
+            command.RegisterCommand(new EngineContainerCommand());
             new CommandEngine(command).Run(args);
         }
     }
@@ -61,6 +67,87 @@ namespace SampleConsole
         {
             for (var i = 0; i < 5; i++)
                 OutputInformation("Some data {0}", i);
+            return true;
+        }
+    }
+
+    public class EngineContainerCommand : ContainerCommandBase
+    {
+        public EngineContainerCommand() 
+            : base("Engine")
+        {
+            RegisterCommand(new WorkingOutputCommand());
+            RegisterCommand(new FailingOutputCommand());
+        }
+    }
+
+    public class WorkingOutputCommand : ActionCommandBase
+    {
+        private readonly Timer _timer;
+
+        public WorkingOutputCommand() 
+            : base("work", "An engine output example that works")
+        {
+            _timer = new Timer { Interval = 3000 };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var rng = new Random();
+            var output = new string('X', rng.Next(3, 10));
+
+            switch (rng.Next(3))
+            {
+                case 0:
+                    OutputInformation(output);
+                    break;
+
+                case 1:
+                    OutputWarning(output);
+                    break;
+
+                case 2:
+                    OutputError(output);
+                    break;
+
+                case 3:
+                    OutputEvent(output);
+                    break;
+            }
+        }
+
+        public async override Task<bool> InvokeAsync(string paramList)
+        {
+            _timer.Start();
+
+            return true;
+        }
+    }
+
+    public class FailingOutputCommand : ActionCommandBase
+    {
+        private readonly Timer _timer;
+
+        public FailingOutputCommand() 
+            : base("fail", "An engine output example that fails")
+        {
+            _timer = new Timer { Interval = 3000 };
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var rng = new Random();
+            var output = new string('X', rng.Next(3, 10));
+
+            Console.WriteLine(output);
+        }
+
+        public async override Task<bool> InvokeAsync(string paramList)
+        {
+            _timer.Start();
+
             return true;
         }
     }
