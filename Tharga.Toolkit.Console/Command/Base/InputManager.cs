52338b16161fa691a9fb63abdc31707c0fe86d50
Daniel Bohlin <daniel.bohlin@gmail.com>
2015-08-24 21:01:58 +0200

    Shift-tab moves backwards in selection lists.

--- Tharga.Toolkit.Console/Command/Base/InputManager.cs
+++ Tharga.Toolkit.Console/Command/Base/InputManager.cs
@@ -171,8 +171,13 @@ namespace Tharga.Toolkit.Console.Command.Base
                             case ConsoleKey.Tab:
                                 if (selection.Any())
                                 {
-                                    var tabIndex = _tabIndex + 1;
+                                    var step = 1;
+                                    if (readKey.Modifiers == ConsoleModifiers.Shift)
+                                        step = -1;
+
+                                    var tabIndex = _tabIndex + step;
                                     if (tabIndex == selection.Length) tabIndex = 0;
+                                    if (tabIndex == -1) tabIndex = selection.Length - 1;
                                     Clear(inputBuffer);
                                     _console.Write(selection[tabIndex].Value);
                                     inputBuffer.Add(selection[tabIndex].Value);
