0dd388654d89cc3e6ee55e520b948cddd28b8fea
poxet <poxet@clowncrew.com>
2023-07-15 12:43:09 +0200

    update namespace

--- Build/AssemblyVersionInfo.cs
+++
@@ -1,4 +0,0 @@
-sing System.Reflection;
-
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
--- Tharga.Console/Properties/AssemblyInfo.cs
+++ Tharga.Console/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //[assembly: AssemblyTitle("Tharga.Toolkit.Console")]
@@ -14,13 +14,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("00e175b1-6820-4adc-b816-0b3096500e38")]
 
-[assembly: InternalsVisibleTo("Tharga.Toolkit.Console.Tests")]
+[assembly: InternalsVisibleTo("Tharga.Console.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
