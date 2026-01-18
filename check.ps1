33d7ea777797450aaf3b128789bc64d6d5cdcc09
Daniel Bohlin <daniel.bohlin@live.se>
2026-01-18 23:18:52 +0100

    chore: add check.ps1

---
+++ check.ps1
@@ -0,0 +1,9 @@
+﻿dotnet --version
+
+# Build the solution (change the .sln name if needed)
+dotnet build
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+# Run tests (will succeed even if there are no tests, but may take time)
+dotnet test
+exit $LASTEXITCODE
