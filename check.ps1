e43699418c09e9e36c12c36a50d2191077d437be
Daniel Bohlin <daniel.bohlin@live.se>
2026-01-18 23:21:00 +0100

    add check file

--- check.ps1
+++ check.ps1
@@ -1,2 +1,9 @@
-cd Sample.Cli
-.\check.ps1
+dotnet --version
+
+# Build the solution (change the .sln name if needed)
+dotnet build
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+# Run tests (will succeed even if there are no tests, but may take time)
+dotnet test
+exit $LASTEXITCODE
